7a189b08a5ecafbb8345cac8319548917857f344
madhunikitac <madhunikitac@packtpub.com>
2018-05-23 13:13:21 +0530

    Code files added

---
+++ Chapter03/My First iOS app/My First iOS app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My First iOS app
+//
+//  Created by Emil Atanasov on 5/8/17.
+//  Copyright © 2017 Appose Studio Inc. All rights reserved.
+//
+
+import UIKit
+//my dummy comment
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
