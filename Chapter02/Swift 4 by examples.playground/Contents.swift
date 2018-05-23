1f69b3b71316df91d79979482fd532db29c2a915
madhunikitac <madhunikitac@packtpub.com>
2018-05-23 13:12:20 +0530

    Code files added

---
+++ Chapter02/Swift 4 by examples.playground/Contents.swift
@@ -0,0 +1,71 @@
+//: Playground - noun: a place where people can play
+// some comment
+import UIKit
+
+
+var str = "Hello, playground"
+
+switch str {
+case "swift":
+    print("Hello, Swift 4!")
+default:
+    print("Who are you?")
+}
+
+var sum = 0
+for i in 1...5 {
+    print("\(i)")
+    
+    sum += i
+}
+
+test("hi")
+
+let img = UIImage(named: "open_xcode.png")
+
+
+//markup
+//: # Header Big
+//: ## Header Normal
+//: ### Header Small
+ 
+ 
+//: List
+//: * option 1
+//: * option 2
+//: * option 3
+//: - option 3.1
+ 
+//: Enumerated list
+//: 1. option 1
+//: 1. option 2
+//: 1. option 3
+ 
+ 
+//: This is _italic_ text.
+ 
+//: This is __bold__ text.
+ 
+//: This is ___bold & italic___ text.
+ 
+//: This is **bold** text.
+ 
+//: The code ```let x = 5``` has different style.
+ 
+//: An example follows:
+ 
+//: var a = 13
+//: var b = a + 7
+ 
+ 
+ 
+//: - Note:
+//:"You can learn Swift 4."
+//: \
+//: \
+//: Playgrounds are really nice place to learn Swift 4.
+ 
+//: ![Image from resources](open_xcode.png)
+
+//: [Swift](https://swift.org)
+
---
+++ Chapter02/Swift 4 by examples.playground/Sources/Functions.swift
@@ -0,0 +1,7 @@
+import Foundation
+/**
+ * Don't forget to add public, to make your functions, vairables, classes, structures or any other types visible to the playground.
+ **/
+public func test(_ test:String) {
+    print("Calling \(#function)")
+}
